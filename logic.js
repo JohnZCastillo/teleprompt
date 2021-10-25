0bbe638611fcf0d84896d44242d9e88237ee9932
John Z Castillo <johnzunigacastillo@gmail.com>
2021-10-25 20:34:41 +0800

    basic 3

--- logic.js
+++ logic.js
@@ -10,10 +10,14 @@ function show(){
 
 function hideDisplay(){
     displaySection.classList.add('hide');
+    inputSection.classList.remove('hide');
+    document.body.style.backgroundColor = "white";
 }
 
 function showDisplay(){
     displaySection.classList.remove('hide');
+    inputSection.classList.add('hide');
+    document.body.style.backgroundColor = "black";
 }
 const toTop = document.querySelector('.totop');
     toTop.addEventListener('click',()=>{
@@ -24,6 +28,7 @@ const toTop = document.querySelector('.totop');
 const input = document.querySelector('.input__content');
 const content = document.querySelector(".display");
 const displaySection = document.querySelector('.display-section');
+const inputSection = document.querySelector('.input');
 
 const hide = document.querySelector('.hideDislay');
     hide.addEventListener('click',()=>{
